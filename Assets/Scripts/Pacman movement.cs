e1404a105c233901e0f8575b1297f7a612360fc3
Jiankun <67788107+Jiankun-L@users.noreply.github.com>
2021-09-25 20:05:12 +1000

    Redraw the wall
    redesign the wall segments

--- Assets/Scripts/Pacman movement.cs
+++ Assets/Scripts/Pacman movement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Pacmanmovement : MonoBehaviour
 {
+    public Animator pacAnimationController;
+
+    public AudioSource theSound;
+
     // Start is called before the first frame update
     void Start()
     {
