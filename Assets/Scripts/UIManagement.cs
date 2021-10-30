0e1ec98dc9bfeeda871ea74421c3d09f65c27fd4
Jiankun <jiankun.li@student.uts.edu.au>
2021-10-30 23:40:30 +1100

    Modify start screen
    Add game intro audio

--- Assets/Scripts/UIManagement.cs
+++ Assets/Scripts/UIManagement.cs
@@ -6,16 +6,21 @@ using UnityEngine.UI;
 
 public class UIManagement : MonoBehaviour
 {
+    public AudioSource startSound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //startSound.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(startSound.isPlaying == false)
+        {
+            startSound.Play();
+        }
     }
 
     public void LoadLevelOne()
